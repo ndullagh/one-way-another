885c0f854253747a1f56d3475ee8d35059f1cc4c
Gannon Bardasian <fbardasian@gmail.com>
2025-03-10 18:45:54 -0700

    Pushing STRAIGHT INTO MAIN WITH NO PROTECTION

--- scenes/main/levels/level 2/Checkpoint.gd
+++ scenes/main/levels/level 2/Checkpoint.gd
@@ -1,16 +1,26 @@
 extends Area2D
 
+@onready var defaultSprite : Sprite2D = $DefaultSprite
+@onready var activatedSprite : Sprite2D = $ActivateSprite
+
 @export var player : CharacterBody2D
 @export var spawnPoint: spawnPoint
+var activated : bool = false 
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
+	activatedSprite.hide()
 	pass # Replace with function body.
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
 		spawnPoint.spawnPointVar = global_position
+		defaultSprite.hide()
+		activatedSprite.show()
+		
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
