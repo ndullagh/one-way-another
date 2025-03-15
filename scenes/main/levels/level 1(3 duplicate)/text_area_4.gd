350796b54a4c8280094505affa50be87363a9df2
ndullagh <113929865+ndullagh@users.noreply.github.com>
2025-03-15 02:28:24 -0700

    whoops

---
+++ scenes/main/levels/level 1(3 duplicate)/text_area_4.gd
@@ -0,0 +1,59 @@
+extends Area2D
+
+#Cutscene 2 for level 3
+
+@onready var textbox = $/root/Node2D/Textbox
+@onready var sprite = $Sprite2D
+@onready var enter_prompt = $AnimatedSprite2D
+
+var player_inside = null
+var cutscene_triggered = false
+
+func _ready():
+	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
+	enter_prompt.hide()
+
+func _on_body_entered(body):
+	#ensure it's the player and that the cutscene hasn't been seen yet
+	if body.is_in_group("Player"): 
+		print("ENTERED ROBOT AREA")
+		print(self)
+		player_inside = body  # Store player reference
+		set_outline(true)
+		enter_prompt.show()
+		enter_prompt.play("default")
+	print(cutscene_triggered)
+		
+		
+func _on_body_exited(body):
+	# Remove reference when player leaves
+	if body == player_inside:
+		player_inside = null
+		set_outline(false)
+		enter_prompt.stop()
+		enter_prompt.hide()
+	print(cutscene_triggered)
+	
+func set_outline(enable: bool):
+	if enable:
+		sprite.self_modulate = Color(1.2, 1.2, 1.2, 1)  # Brighten the sprite slightly
+	else:
+		sprite.self_modulate = Color(1, 1, 1, 1)  # Reset to normal colo
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	# Check for input only if player is inside
+	if player_inside and Input.is_action_just_pressed("Enter") and not cutscene_triggered:
+		cutscene_triggered = true
+		GameState.mark_dialogue_as_seen(2)  # Mark as seen
+
+		textbox.queue_text("Kol: It's an old war machine - looks just like the others around here.", "res://cutscenes/robot.png")
+		textbox.queue_text("It's weird how many of them are still fighting...")
+		textbox.queue_text("Yeesh, if the war's still going then I'm in BIG trouble for deserting.")
+		print("QUEUED ROBOT DIALOGUE")
+		
+	if textbox.current_state == textbox.State.READY and textbox.text_queue.is_empty():
+		cutscene_triggered = false
+		
+	
